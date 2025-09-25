8c25ce7935ae2418a28d8ad96be0927620f9ad95
Peter <xervipeter@gmail.com>
2025-09-25 18:55:50 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  const paragraph = document.querySelector("p");
+  if (paragraph) {
+    paragraph.textContent = "This is really cool!";
+  }
+});
